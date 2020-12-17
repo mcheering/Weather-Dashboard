7434381de02e17621f9543d58a19fc8659d47885
Matthew Heering <matthew.c.heering01@gmail.com>
2020-12-16 19:44:18 -0600

    adding updated files from local machine with finished project.  This includes updates to html, css, and js files.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Weather-Dashboard
+<!DOCTYPE html>
+<html> 
+      <head>
+            <h2>Weather Dashboard</h2>
+      </head>
+      <body>
+            <h5>Background</h5>
+            <p>This project uses html, css, javascript/jQuery, and the openweather API to allow users to search cities and the dashboard return current weather along with a 5 day forecast.  The most challenging part of this project was to figure out how not to display duplicate cities.  AN early bug was a search for the same city would yield multiple recent search cards for that city.  However, this was fixed by using local storage and comparing strings to remove duplicates. Overall this project was a challenge to build</p>
+            <h5>Tutorial</h5>
+            <p>The first time you land on the page, you will be greeted with blank cards like in the image below</p>
+            <img src="images/landing.png" style="width=350px;">
+            <p>Click into the search bar.  If you do not type a city and leave the search blank when you click the search button, you will receive an error message. </p>
+            <img src="images/emptySearch.png" style="width=350px;">
+            <p>After clearing the message, type in a valid city name, and click search and it will give you the current weather along with a 5 day forecast.</p>
+            <img src="images/searchCity.png" style="width=350px;">
+            <p>When you search you will see a bluish-green card display below the clear button.  For every unique city you search, a new card will display.  If you search same city twice, a new card will not pop up.</p>
+            <img src="images/multipleCities.png" style="width=350px;">
+            <p>There is not a limit to how many cities you can add, but if you ever want to clear you search history, there is a button to clear everything.  This will also clear the cards on the page to get rid of the data being displayed.</p>
+            <img src="images/afterClear.png" style="width=350px;">
+            <h5>Potential Add-ons</h5>
+            <p>In the future I could make it so that you can delete individual history items.  I would also like to find a way to pull images that are relatively current for a location so you can see what it looks like. </p>
+      </body>
+</html>
---
+++ script.js
@@ -0,0 +1,120 @@
+$("document").ready(function () {
+      var recentCities = JSON.parse(localStorage.getItem("cities")) || []
+      function getWeather(cityName) {
+            var currentWthrURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=af1a956362d5886e7e0d1d06e4b5d0a0"
+            $.ajax({
+                  url: currentWthrURL,
+                  method: "GET"
+            }).then(function (response) {
+                  console.log(response)
+                  var now = moment()
+                  var future1 = moment().add(1, 'days').format("MM/DD")
+                  var future2 = moment().add(2, 'days)').format("MM/DD")
+                  var future3 = moment().add(3, 'days').format("MM/DD")
+                  var future4 = moment().add(4, 'days').format("MM/DD")
+                  var future5 = moment().add(5, 'days').format("MM/DD")
+
+                  $("#wthrIcn").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png")
+                  $("#city-date-wthr-icon").text(response.name + ", " + response.sys.country + ", " + now.format("MMM Do YYYY, h:mm a"))
+                  $("#currentTemp").text("Temperature: " + Math.floor(response.main.temp) + " \u00B0" + "F")
+                  $("#currentHum").text("Humidity: " + response.main.humidity + "%")
+                  $("#currentSpeed").text("Wind Speed: " + response.wind.speed + " mph")
+
+                  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=af1a956362d5886e7e0d1d06e4b5d0a0"
+
+                  $.ajax({
+                        url: uvURL,
+                        method: "GET"
+                  }).then(function (uvResponse) {
+                        console.log(uvResponse)
+                        $("#currentUV").text("UV Index: ")
+                        $("#uvText").text(uvResponse.value)
+                        $("#uvText").attr("style", "background-color: #DB5461; color: white; border-radius: 6px; padding: 2px 4px;")
+                  })
+                  //Dates for forecast cards
+                  $("#future1day").text(future1)
+                  $("#future2day").text(future2)
+                  $("#future3day").text(future3)
+                  $("#future4day").text(future4)
+                  $("#future5day").text(future5)
+                  //weather symbols, temps, annd humidity for forecast cards
+                  var foreCastUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&units=imperial&cnt=5&appid=af1a956362d5886e7e0d1d06e4b5d0a0"
+                  $.ajax({
+                        url: foreCastUrl,
+                        method: "GET"
+                  }).then(function (future) {
+                        console.log(future)
+
+                        $("#futurewthrSym1").attr("src", "http://openweathermap.org/img/wn/" + future.daily[0].weather[0].icon + ".png")
+                        $("#futurewthrSym2").attr("src", "http://openweathermap.org/img/wn/" + future.daily[1].weather[0].icon + ".png")
+                        $("#futurewthrSym3").attr("src", "http://openweathermap.org/img/wn/" + future.daily[2].weather[0].icon + ".png")
+                        $("#futurewthrSym4").attr("src", "http://openweathermap.org/img/wn/" + future.daily[3].weather[0].icon + ".png")
+                        $("#futurewthrSym5").attr("src", "http://openweathermap.org/img/wn/" + future.daily[4].weather[0].icon + ".png")
+
+                        $("#futurehigh1").text("High: " + Math.floor(future.daily[0].temp.max) + " \u00B0" + "F")
+                        $("#futurehigh2").text("High: " + Math.floor(future.daily[1].temp.max) + " \u00B0" + "F")
+                        $("#futurehigh3").text("High: " + Math.floor(future.daily[2].temp.max) + " \u00B0" + "F")
+                        $("#futurehigh4").text("High: " + Math.floor(future.daily[3].temp.max) + " \u00B0" + "F")
+                        $("#futurehigh5").text("High: " + Math.floor(future.daily[4].temp.max) + " \u00B0" + "F")
+
+                        $("#futurelow1").text("Low: " + Math.floor(future.daily[0].temp.min) + " \u00B0" + "F")
+                        $("#futurelow2").text("Low: " + Math.floor(future.daily[1].temp.min) + " \u00B0" + "F")
+                        $("#futurelow3").text("Low: " + Math.floor(future.daily[2].temp.min) + " \u00B0" + "F")
+                        $("#futurelow4").text("Low: " + Math.floor(future.daily[3].temp.min) + " \u00B0" + "F")
+                        $("#futurelow5").text("Low: " + Math.floor(future.daily[4].temp.min) + " \u00B0" + "F")
+
+                        $("#futureHum1").text("Humidity: " + future.daily[0].humidity + "%")
+                        $("#futureHum2").text("Humidity: " + future.daily[1].humidity + "%")
+                        $("#futureHum3").text("Humidity: " + future.daily[2].humidity + "%")
+                        $("#futureHum4").text("Humidity: " + future.daily[3].humidity + "%")
+                        $("#futureHum5").text("Humidity: " + future.daily[4].humidity + "%")
+                  })
+            })
+      }
+
+      $("button").on("click", function () {
+            const cityName = $("#locationInput").val()
+            if (cityName == "") {
+                  alert("Enter a valid city")
+                  return
+            } else {
+                  getWeather(cityName)
+                  recentCities.push(cityName)
+                  localStorage.setItem("cities", JSON.stringify(recentCities))
+                  renderSearchHistory()
+            }
+
+      })
+
+      $("#clearButton").on("click", function () {
+            checkDupCities = []
+            uniqueCities = []
+            recentCities = []
+            localStorage.clear()
+            $("input").remove("#recentCity")
+            window.location.reload()
+      })
+
+      function renderSearchHistory() {
+            const historyEl = $("#recentSearchSection").html("")
+            var retrievedCities = localStorage.getItem("cities")
+            var checkDupCities = JSON.parse(retrievedCities)
+            let uniqueCities = [...new Set(checkDupCities)]
+            for (let i = 0; i < uniqueCities.length; i++) {
+                  const historyItem = $("<input>")
+                  historyItem.attr("type", "text")
+                  historyItem.attr("readonly", true)
+                  historyItem.attr("id", "recentCity")
+                  historyItem.attr("value", uniqueCities[i])
+                  historyItem.on("click", function () {
+                        getWeather(historyItem.val())
+                  })
+                  historyEl.append(historyItem)
+            } if (uniqueCities.length > 0) {
+                  getWeather(uniqueCities[uniqueCities.length - 1])
+            }
+      }
+      renderSearchHistory()
+
+
+})
